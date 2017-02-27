b6e3f78b146cd4499028882bdf4c4654da661b5a
Burak Emre Kabakcı <emrekabakci@gmail.com>
2017-02-27 15:19:28 +0300

    Fix if a method that returns a value is called when rakam is not initialized

--- src/rakam-snippet.js
+++ src/rakam-snippet.js
@@ -3,15 +3,16 @@
     var as = document.createElement('script');
     as.type = 'text/javascript';
     as.async = true;
-    as.src = 'https://cdn.rawgit.com/buremba/rakam-javascript/master/rakam.js';
-    as.onload = function() {window.rakam.runQueuedFunctions();};
+    as.src = 'https://d2f7xo8n6nlhxf.cloudfront.net/rakam.min.js';
+    as.onload = function () {window.rakam.runQueuedFunctions();};
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(as, s);
 
 
     function proxy(obj, fn) {
-        obj[fn] = function() {
-            this._q.push([fn].concat(Array.prototype.slice.call(arguments, 0))); return this;
+        obj[fn] = function () {
+            this._q.push([fn].concat(Array.prototype.slice.call(arguments, 0)));
+            return this;
         };
     }
 
@@ -28,10 +29,23 @@
     rakam._q = [];
 
     var funcs = ["init", "logEvent", "logInlinedEvent", "setUserId", "getUserId", "getDeviceId", "setSuperProperties",
-        "setOptOut", "setVersionName", "setDomain", "setUserProperties", "setDeviceId", "onload",
-        "onEvent", "getTimeOnPreviousPage", "getTimeOnPage", "startTimer", "isReturningUser"];
+                 "setOptOut", "setVersionName", "setDomain", "setUserProperties", "setDeviceId", "onload",
+                 "onEvent", "startTimer"];
     for (var j = 0; j < funcs.length; j++) {
         proxy(rakam, funcs[j]);
     }
+    var nofuncs = ["getTimeOnPreviousPage", "getTimeOnPage", "isReturningUser"];
+    var errFunc = (window.console ? (window.console.error || window.console.log) : null) || function () {};
+    var notifyFunc = function (funcName) {
+        return function () {
+            errFunc("The method rakam." + funcName + "() must be called inside rakam.init callback function!");
+        };
+    };
+
+    for (j = 0; j < nofuncs.length; j++) {
+        rakam[nofuncs[j]] = notifyFunc(nofuncs[j]);
+        proxy(rakam, nofuncs[j]);
+    }
+
     window.rakam = rakam;
 })(window, document);
